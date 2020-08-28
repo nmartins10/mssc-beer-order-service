35b99c81777c0a76797bdb56808208822fd23f2c
Nuno Martins <nuno.martins@externals.adidas.com>
2020-08-28 14:31:51 +0100

    refactor statemachine

--- src/main/java/guru/sfg/beer/order/service/domain/BeerOrderStatusEnum.java
+++ src/main/java/guru/sfg/beer/order/service/domain/BeerOrderStatusEnum.java
@@ -20,7 +20,9 @@ package guru.sfg.beer.order.service.domain;
 public enum BeerOrderStatusEnum {
     NEW,
     VALIDATED,
+    VALIDATION_PENDING,
     VALIDATION_EXCEPTION,
+    ALLOCATION_PENDING,
     ALLOCATED,
     ALLOCATION_EXCEPTION,
     PENDING_INVENTORY,
